f4214cd806246938a2176705cc8f7b3b9cf2af3d
annkay108 <annkhachhibhoya@gmail.com>
2020-08-04 08:31:07 +0545

    mongoose connection established

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,6 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv = require("dotenv");
+dotenv.config();
 // import logger from "morgan";
 // import cors from "cors";
 const indexRoutes_1 = __importDefault(require("./routes/indexRoutes"));
@@ -14,16 +17,25 @@ class Server {
         this.routes();
     }
     config() {
+        // Mongoose connection
+        mongoose_1.default.connect(process.env.MONGODB_URI, {
+            keepAlive: true,
+            useNewUrlParser: true,
+            reconnectTries: Number.MAX_VALUE,
+            useUnifiedTopology: true
+        })
+            .then(() => console.log("Connected to database"))
+            .catch((err) => console.log(err));
         // Setting the port value
-        this.app.set('port', process.env.PORT || 3000);
+        this.app.set("port", process.env.PORT || 5000);
     }
     routes() {
         const router = express_1.default.Router();
-        this.app.use('/', indexRoutes_1.default);
+        this.app.use("/", indexRoutes_1.default);
     }
     start() {
-        this.app.listen(this.app.get('port'), () => {
-            console.log('Server is listening on port', this.app.get('port'));
+        this.app.listen(this.app.get("port"), () => {
+            console.log("Server is listening on port", this.app.get("port"));
         });
     }
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,31 +1,42 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import mongoose from "mongoose";
+import dotenv = require('dotenv');
+dotenv.config();
 // import logger from "morgan";
 // import cors from "cors";
 
 import indexRoutes from "./routes/indexRoutes";
 
-class Server{
+class Server {
   public app: express.Application;
-  constructor(){
+  constructor() {
     this.app = express();
     this.config();
     this.routes();
   }
-  public config(): void{
+  public config(): void {
+    // Mongoose connection
+    mongoose.connect(process.env.MONGODB_URI, {
+      keepAlive: true,
+      useNewUrlParser: true,
+      reconnectTries: Number.MAX_VALUE,
+      useUnifiedTopology: true
+    })
+    .then(()=>console.log("Connected to database"))
+    .catch((err)=>console.log(err));
     // Setting the port value
-    this.app.set('port', process.env.PORT||3000);
+    this.app.set("port", process.env.PORT || 5000);
   }
 
-  public routes():void{
+  public routes(): void {
     const router: express.Router = express.Router();
 
-    this.app.use('/',indexRoutes);
+    this.app.use("/", indexRoutes);
   }
 
-  public start():void{
-    this.app.listen(this.app.get('port'),()=>{
-      console.log('Server is listening on port', this.app.get('port'));
+  public start(): void {
+    this.app.listen(this.app.get("port"), () => {
+      console.log("Server is listening on port", this.app.get("port"));
     });
   }
 }
