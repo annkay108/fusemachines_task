0b4501c19d7d96d31b262babff058e7aecad0b36
annkay108 <annkhachhibhoya@gmail.com>
2020-08-06 12:53:36 +0545

    failed to implement remove

--- client/src/components/UpdateFileListInput.tsx
+++ client/src/components/UpdateFileListInput.tsx
@@ -24,7 +24,7 @@ const UpdateFileListInput = (props:any) => {
             ))
             }
             {console.log('render updateFile')}
-        </div>
+
     )
 }
 export default UpdateFileListInput;
