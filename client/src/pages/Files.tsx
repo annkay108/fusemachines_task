9902d7b791d81d7f959303c2db1e7ee029011c9f
annkay108 <annkhachhibhoya@gmail.com>
2020-08-07 23:29:03 +0545

    implemented pagination on files

--- client/src/pages/Files.tsx
+++ client/src/pages/Files.tsx
@@ -15,8 +15,10 @@ interface IFile{
 }
 
 const Files = (props:any) => {
-    const [fileArr, setFileArr] = useState<IFile[]|null>(null);
+    const [fileArr, setFileArr] = useState<IFile[]>([]);
     const [ modalShow, setModalShow ] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [filesPerPage] = useState(10);
 
     useEffect(()=>{
         fileService.getListOfFilesByCourseId(props.match.params.id)
@@ -30,28 +32,56 @@ const Files = (props:any) => {
         .catch(err => {throw err})
     }
 
+    const paginate = (number:number)=> setCurrentPage(number);
+
+    const indexOfLastFile = currentPage * filesPerPage;
+    const indexOfFirstFile = indexOfLastFile - filesPerPage;
+    const currentFiles = fileArr.reverse().slice(indexOfFirstFile, indexOfLastFile);
+
+    const pageNumbers = [];
+    for (let i = 1; i<=Math.ceil(fileArr.length/filesPerPage);i++){
+        pageNumbers.push(i)
+    }
     return (
         <div>
             <SideBar courseId={props.match.params.id}/>
+            <Button variant="primary" onClick={() => setModalShow(true)}>
+                Add File
+            </Button>
             <div>
                 {
                     fileArr?
                     fileArr.length?
-                        fileArr.map(el=>{
-                            return(
-                                <div key={el._id}>
-                                    {el.fileName}
-                                    <a href={`localhost:5000/file/${el._id}/download`} target="_blank" download>Download</a>
-                                </div>
+                    <ul className="list-group mb-4">
+                        {
+                            currentFiles.map(el=>{
+                                return(
+                                    <li key={el._id} className="list-group-item">
+                                        {el.fileName}
+                                        <a href={`localhost:5000/file/${el._id}/download`} target="_blank" download>Download</a>
+                                    </li>
+                                )
+                                }
                             )
-                        })
+                        }
+                    </ul>
                     :<h1>No File</h1>
                     :null
                 }
             </div>
-            <Button variant="primary" onClick={() => setModalShow(true)}>
-                Add File
-            </Button>
+            <nav>
+                <ul className = "pagination">
+                    {
+                        pageNumbers.map(number =>{
+                            return(
+                            <li key={number} className="page-item">
+                                <a onClick={()=>paginate(number)} className="page-link">{number}</a>
+                            </li>)
+                        }
+                        )
+                    }
+                </ul>
+            </nav>
             <AddFile 
                 courseId={props.match.params.id}
                 show={modalShow}
